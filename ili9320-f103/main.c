1044f13b660890fb1e3c4400d63f155979bc838f
Dmitry Semernin <dsemernin@gmail.com>
2013-06-13 08:46:21 +0300

    ili9320 code polished; +full color BMP

--- ili9320-f103/main.c
+++ ili9320-f103/main.c
@@ -4,16 +4,20 @@
 #include <stm32f10x_spi.h>
 #include <delay.h>
 
+
 #define SPI_START 0x70   /* Start byte for SPI transfer */
 #define SPI_RD    0x01   /* WR bit 1 within start */
 #define SPI_WR    0x00   /* WR bit 0 within start */
 #define SPI_DATA  0x02   /* RS bit 1 within start byte */
 #define SPI_INDEX 0x00   /* RS bit 0 within start byte */
 
+
 #define  ASCII_8X16_MS_Gothic
 //#define  ASCII_8X16_System
 #include <fonts.h>
 #include <facepalm.h>
+#include <face565.h>
+
 
 uint8_t SPI_SendRecv(uint8_t byte) {
 	while (SPI_I2S_GetFlagStatus(SPI1,SPI_I2S_FLAG_TXE) == RESET);
@@ -61,31 +65,38 @@ void LCD_WriteReg(uint16_t _reg, uint16_t _data) {
 	LCD_WriteData(_data);
 }
 
-void LCD_SetCursor(uint16_t X, uint16_t Y) {
-	LCD_WriteReg(0x0020,239-Y);
-	LCD_WriteReg(0x0021,X);
-}
-
-void LCD_WriteData_Only(uint16_t data) {
+void LCD_WriteDataOnly(uint16_t data) {
 	SPI_SendRecv(data >> 8);
 	SPI_SendRecv(data & 0xff);
 }
 
-void LCD_SetPoint(uint16_t X, uint16_t Y, uint16_t C) {
+void LCD_SetCursor(uint16_t X, uint16_t Y) {
+	LCD_WriteReg(0x0020,Y);
+	LCD_WriteReg(0x0021,X);
+}
+
+void LCD_Pixel(uint16_t X, uint16_t Y, uint16_t C) {
 	LCD_SetCursor(X,Y);
 	LCD_WriteReg(0x0022,C);
 }
 
+void LCD_SetWindow(uint16_t X, uint16_t Y, uint16_t W, uint16_t H) {
+	uint16_t XW = X + W - 1;
+	uint16_t YH = Y + H - 1;
+	LCD_SetCursor(X,Y);
+	LCD_WriteReg(0x0050,Y);  // Top RAM address pos
+	LCD_WriteReg(0x0051,YH); // Bottom RAM address pos
+	LCD_WriteReg(0x0052,X);  // Left RAM address pos
+	LCD_WriteReg(0x0053,XW); // Right RAM address pos
+}
+
 void LCD_Clear(uint16_t color) {
 	uint32_t i = 0;
-	LCD_WriteReg(0x0020,0);
-	LCD_WriteReg(0x0021,0);
+	LCD_SetWindow(0,0,320,240);
 	LCD_WriteIndex(0x0022);
 	GPIO_WriteBit(GPIOA,GPIO_Pin_2,Bit_RESET); // SPI_CS_LOW
 	SPI_SendRecv(SPI_START | SPI_WR | SPI_DATA);
-	for (i = 0; i < 320*240; i++) {
-		LCD_WriteData_Only(color);
-	}
+	for (i = 0; i < 320*240; i++) { LCD_WriteDataOnly(color); }
 	GPIO_WriteBit(GPIOA,GPIO_Pin_2,Bit_SET);   // SPI_CS_HIGH
 }
 
@@ -93,29 +104,67 @@ uint16_t RGB888to565(uint8_t R,uint8_t G,uint8_t B) {
 	return ((R >> 3) << 11) | ((G >> 2) << 5) | (B >> 3);
 }
 
-void GetASCIICode(uint8_t* pBuffer, uint8_t ASCII) {
-	memcpy(pBuffer,AsciiLib[ASCII-32],16);
+void LCD_HLine(uint16_t X0, uint16_t X1, uint16_t Y, uint16_t Color) {
+	uint16_t W = X1 - X0 + 1;
+    uint16_t i;
+
+    LCD_SetWindow(X0,Y,W,1);
+    LCD_WriteIndex(0x0022);
+	GPIO_WriteBit(GPIOA,GPIO_Pin_2,Bit_RESET); // SPI_CS_LOW
+	SPI_SendRecv(SPI_START | SPI_WR | SPI_DATA);
+	for (i = 0; i < W; i++) { LCD_WriteDataOnly(Color); }
+	GPIO_WriteBit(GPIOA,GPIO_Pin_2,Bit_SET);   // SPI_CS_HIGH
+}
+
+void LCD_VLine(uint16_t X, uint16_t Y0, uint16_t Y1, uint16_t Color) {
+	uint16_t H = Y1 - Y0 + 1;
+	uint16_t i;
+
+	LCD_SetWindow(X,Y0,1,H);
+	LCD_WriteIndex(0x0022);
+	GPIO_WriteBit(GPIOA,GPIO_Pin_2,Bit_RESET); // SPI_CS_LOW
+	SPI_SendRecv(SPI_START | SPI_WR | SPI_DATA);
+	for (i = 0; i < H; i++) { LCD_WriteDataOnly(Color); }
+	GPIO_WriteBit(GPIOA,GPIO_Pin_2,Bit_SET);   // SPI_CS_HIGH
+}
+
+void LCD_Rect(uint16_t X, uint16_t Y, uint16_t W, uint16_t H, uint16_t Color) {
+	LCD_HLine(X,X+W-1,Y,Color);
+	LCD_HLine(X,X+W-1,Y+H-1,Color);
+	LCD_VLine(X,Y,Y+H-1,Color);
+	LCD_VLine(X+W-1,Y,Y+H-1,Color);
+}
+
+void LCD_FillRect(uint16_t X, uint16_t Y, uint16_t W, uint16_t H, uint16_t Color) {
+	uint32_t i = 0;
+	LCD_SetWindow(X,Y,W,H);
+	LCD_WriteIndex(0x0022);
+	GPIO_WriteBit(GPIOA,GPIO_Pin_2,Bit_RESET); // SPI_CS_LOW
+	SPI_SendRecv(SPI_START | SPI_WR | SPI_DATA);
+	for (i = 0; i < W*H; i++) { LCD_WriteDataOnly(Color); }
+	GPIO_WriteBit(GPIOA,GPIO_Pin_2,Bit_SET);   // SPI_CS_HIGH
 }
 
 void LCD_PutChar(uint16_t X, uint16_t Y, uint8_t Char, uint16_t chColor, uint16_t bgColor) {
 	uint16_t i,j;
 	uint8_t buffer[16],tmpCh;
 
-    GetASCIICode(buffer,Char);
+    LCD_SetWindow(X,Y,8,16);
+    memcpy(buffer,AsciiLib[Char-32],16);
 
     for (i = 0; i < 16; i++) {
     	tmpCh = buffer[i];
     	for (j = 0; j < 8; j++) {
     		if (((tmpCh >> (7-j)) & 0x01) == 0x01) {
-        		LCD_SetPoint(X+j,Y+i,chColor);
+        		LCD_Pixel(X+j,Y+i,chColor);
   			}
-//    		LCD_SetPoint(X+j,Y+i,((tmpCh >> (7-j)) & 0x01) == 0x01 ? chColor : bgColor);
     	}
     }
 }
 
 void LCD_Text(uint16_t X, uint16_t Y, char *str, uint16_t chColor, uint16_t bgColor) {
     unsigned short tmpCh;
+
     do {
         tmpCh = *str++;
         LCD_PutChar(X,Y,tmpCh,chColor,bgColor);
@@ -127,19 +176,32 @@ void LCD_BMP_Mono(uint16_t X, uint16_t Y, uint16_t W, uint16_t H, const uint8_t 
 	uint16_t i,j,k;
 	uint8_t buffer[W];
 
+	LCD_SetWindow(X,Y,W*8,H);
 	for (i = 0; i < H; i++) {
 		memcpy(buffer,FacePalm[i],W);
-//		GetBMPCode(buffer,i,W);
 		for (j = 0; j < W; j++) {
 			for (k = 0; k < 8; k++) {
-				if (((buffer[j] >> (7-k)) & 0x01) == 0x01) {
-					LCD_SetPoint(X+(j << 3)+k,Y+i,Color);
-//				} else {
-//					LCD_SetPoint(X+(j << 3)+k,Y+i,65535-Color);
+				if ((buffer[j] >> (7-k)) & 0x01) {
+					LCD_Pixel(X+(j << 3)+k,Y+i,Color);
 				}
 			}
 		}
 	}
+}
+
+void LCD_BMP(uint16_t X, uint16_t Y, uint16_t W, uint16_t H, const uint16_t *pBMP) {
+	uint16_t i,j;
+
+	LCD_SetWindow(X,Y,W,H);
+	LCD_WriteIndex(0x0022);
+	GPIO_WriteBit(GPIOA,GPIO_Pin_2,Bit_RESET); // SPI_CS_LOW
+	SPI_SendRecv(SPI_START | SPI_WR | SPI_DATA);
+	for (i = 0; i < H; i++) {
+		for (j = 0; j < W; j++) {
+			LCD_WriteDataOnly(Face565[i*80+j]);
+		}
+	}
+	GPIO_WriteBit(GPIOA,GPIO_Pin_2,Bit_SET);   // SPI_CS_HIGH
 }
 
 
@@ -197,80 +259,75 @@ int main(void)
 	GPIO_WriteBit(GPIOA,GPIO_Pin_3,Bit_SET);   // LCD_nRESET = 1
 	Delay_ms(50);                              // Wait 50ms -- necessary!
 
-	uint16_t DevCode = LCD_ReadReg(0x0000);
-
 	// Init LCD
-	LCD_WriteReg(0x0000,0x0000);
-	LCD_WriteReg(0x0001,0x0100);
-	LCD_WriteReg(0x0002,0x0700);
-	LCD_WriteReg(0x0003,0x1008);
-	LCD_WriteReg(0x0004,0x0000);
-	LCD_WriteReg(0x0008,0x0202);
-	LCD_WriteReg(0x0009,0x0000);
-	LCD_WriteReg(0x000a,0x0000);
-	LCD_WriteReg(0x000c,(1<<0));
-	LCD_WriteReg(0x000d,0x0000);
-	LCD_WriteReg(0x000f,0x0000);
+	LCD_WriteReg(0x0000,0x0000); // Stop oscillator
+	LCD_WriteReg(0x0001,(0<<8)); // Driver output ctrl: shift S720 -> S1
+	LCD_WriteReg(0x0002,(1<<10)|(1<<9)|(1<<8)); // LCD driving wave ctrl: Line inversion
+	LCD_WriteReg(0x0003,(1<<12)|(1<<9)|(1<<5)|(1<<4)|(1<<3)); // Entry mode: Vert; H:dec; V:dec; RGB->BGR swap on
+	LCD_WriteReg(0x0004,0x0000); // Scaling: off
+	LCD_WriteReg(0x0008,(1<<9) | (1<<1)); // Display ctrl 2: 2 lines for back and front porch
+	LCD_WriteReg(0x0009,0x0000); // Display ctrl 3: Scan mode: normal; Scan cycle: 0frame
+	LCD_WriteReg(0x000a,0x0000); // Display ctrl 4: FMARK: disabled
+	LCD_WriteReg(0x000c,(1<<0)); // RGB interface ctrl 1: 6-bit RGB; Internal sysclock/VSYNC;
+	LCD_WriteReg(0x000d,0x0000); // Frame marker position: FMARK: 0th line
+	LCD_WriteReg(0x000f,0x0000); // RGB interface ctrl 2
 	Delay_ms(50);
 
-	LCD_WriteReg(0x0007,0x0101);
+	LCD_WriteReg(0x0007,(1<<8) | (1<<0)); // Display control: VCOM to GND; Internal operations On
 	Delay_ms(50);
 
-	LCD_WriteReg(0x0010,(1<<12)|(0<<8)|(1<<7)|(1<<6)|(0<<4));
-	LCD_WriteReg(0x0011,0x0007);
-	LCD_WriteReg(0x0012,(1<<8)|(1<<4)|(0<<0));
-	LCD_WriteReg(0x0013,0x0b00);
-	LCD_WriteReg(0x0029,0x0000);
-	LCD_WriteReg(0x002b,(1<<14));
-	LCD_WriteReg(0x0050,0x0000);
-	LCD_WriteReg(0x0051,239);
-	LCD_WriteReg(0x0052,0x0000);
-	LCD_WriteReg(0x0053,319);
+	LCD_WriteReg(0x0010,(1<<12)|(1<<7)|(1<<6)); // Power control 1: SAP: on; Power supply: on; Current Amplifiers = 1.0
+	LCD_WriteReg(0x0011,(1<<2)|(1<<1)|(1<<0)); // Power control 2: disable stepup frequency
+	LCD_WriteReg(0x0012,(1<<8)|(1<<4)); // Power control 3: VGL:on; internal VcomH
+	LCD_WriteReg(0x0013,(1<<11)|(1<<9)|(1<<8)); // Power control 4: VREG1OUTx0.91
+	LCD_WriteReg(0x0029,0x0000); // Power control 7
+	LCD_WriteReg(0x002b,(1<<5)|(0<<4)); // internal resistor for oscillator; 120Hz frame rate
+	LCD_WriteReg(0x0050,0); // Left RAM address pos
+	LCD_WriteReg(0x0051,239); // Right RAM address pos
+	LCD_WriteReg(0x0052,0); // Top RAM address pos
+	LCD_WriteReg(0x0053,319); // Bottom RAM address pos
 	Delay_ms(50);
 
-	LCD_WriteReg(0x0060,0x2700);
-	LCD_WriteReg(0x0061,0x0001);
-	LCD_WriteReg(0x006a,0x0000);
+	LCD_WriteReg(0x0060,(0<<15)|(1<<13)|(1<<10)|(1<<9)|(1<<8)); // Gate scan ctrl: 320 lines
+	LCD_WriteReg(0x0061,(1<<0)); // Gate scan ctrl: grayscale inversion: on; scan: G1->G320; scrolling: off
+	LCD_WriteReg(0x006a,0x0000); // Gate scan ctrl: scroll 0 lines
 
-	LCD_WriteReg(0x0080,0x0000);
-	LCD_WriteReg(0x0081,0x0000);
-	LCD_WriteReg(0x0082,0x0000);
-	LCD_WriteReg(0x0083,0x0000);
-	LCD_WriteReg(0x0084,0x0000);
-	LCD_WriteReg(0x0085,0x0000);
+	LCD_WriteReg(0x0080,0x0000); // Partial image 1 display position
+	LCD_WriteReg(0x0081,0x0000); // Partial image 1 RAM start address
+	LCD_WriteReg(0x0082,0x0000); // Partial image 1 RAM end address
+	LCD_WriteReg(0x0083,0x0000); // Partial image 2 display position
+	LCD_WriteReg(0x0084,0x0000); // Partial image 2 RAM start address
+	LCD_WriteReg(0x0085,0x0000); // Partial image 2 RAM end address
 
-	LCD_WriteReg(0x0090,(0<<7)|(16<<0));
-	LCD_WriteReg(0x0092,0x0000);
-	LCD_WriteReg(0x0093,0x0001);
-	LCD_WriteReg(0x0095,0x0110);
-	LCD_WriteReg(0x0097,(0<<8));
-	LCD_WriteReg(0x0098,0x0000);
-	LCD_WriteReg(0x0007,0x0133);
+	LCD_WriteReg(0x0090,0x0000); // Panel interface ctrl 1: Clocks/Line: off; Clck=Fosc/1
+	LCD_WriteReg(0x0092,0x0000); // Panel interface ctrl 2: Gate non-overlap period: 0 clocks
+	LCD_WriteReg(0x0093,(1<<1)); //	Panel interface ctrl 3: Source output position: 1 clock
+	LCD_WriteReg(0x0095,(1<<8)|(1<<4)); // Panel interface ctrl 4: 12 DOTCLKs
+	LCD_WriteReg(0x0097,0x0000); // Panel interface ctrl 5: Gate non-overlap period: 0 clocks
+	LCD_WriteReg(0x0098,0x0000); // Panel interface ctrl 6: Source output position: 0 clocks
+	LCD_WriteReg(0x0007,(1<<8)|(1<<5)|(1<<4)|(1<<1)|(1<<0)); // Display control: BASEE=1; Normal display on
 	Delay_ms(100);
-
-	DevCode = LCD_ReadReg(0x0000);
-
-	GPIO_WriteBit(GPIOC,DevCode == 0x0 ? GPIO_Pin_9 : GPIO_Pin_8,Bit_SET);
 
 //	LCD_Clear(RGB888to565(255,0,0)); // RED
 //	LCD_Clear(RGB888to565(0,255,0)); // GREEN
 //	LCD_Clear(RGB888to565(0,0,255)); // BLUE
-	LCD_Clear(0); // BLACK
+	LCD_Clear(RGB888to565(0,0,0)); // BLACK
 
-	LCD_SetPoint(0,0,RGB888to565(255,0,0));
-	LCD_SetPoint(319,239,RGB888to565(0,0,255));
+	LCD_Rect(0,0,320,240,RGB888to565(255,255,255));
 
-	LCD_Text(10,10,"Hello world!",RGB888to565(255,0,255),RGB888to565(0,0,0));
-	LCD_Text(10,38,"Look at this extremally slow screen :(",RGB888to565(0,255,255),RGB888to565(32,64,32));
-	LCD_Text(40,60,"serial interface totally SUXX",RGB888to565(255,255,0),RGB888to565(0,0,0));
-	LCD_Text(50,80,"... and 65K colors only ...",RGB888to565(255,128,0),RGB888to565(0,0,0));
-//	LCD_Text(230,200,"/FACEPALM",RGB888to565(255,55,55),RGB888to565(32,64,16));
+	LCD_Text(5,5,"Hello world!",RGB888to565(255,0,255),RGB888to565(0,100,0));
+	LCD_Text(6,32,"Look at this extremally slow screen :(",RGB888to565(0,255,255),RGB888to565(32,64,32));
+	LCD_Text(40,50,"serial interface totally SUXX",RGB888to565(255,255,0),RGB888to565(0,0,0));
+	LCD_Text(50,70,"... and 65K colors only ...",RGB888to565(255,128,0),RGB888to565(0,0,0));
 
-	LCD_BMP_Mono(120,120,26,120,&FacePalm[0][0],RGB888to565(0,162,232));
+	LCD_BMP_Mono(118,118,26,120,&FacePalm[0][0],RGB888to565(0,162,232));
 
-	LCD_Text(20,190,"(c) Dimon",RGB888to565(255,55,55),RGB888to565(32,64,16));
+	LCD_Text(5,220,"(c) Dimon",RGB888to565(255,55,55),RGB888to565(32,64,16));
 
-    while(1)
-    {
+	LCD_Rect(19,99,82,98,RGB888to565(0,0,255));
+	LCD_BMP(20,100,80,96,&Face565[0]);
+	LCD_Text(106,102,"< 16-bit color",RGB888to565(255,255,0),RGB888to565(0,0,0));
+
+	while(1) {
     }
 }
