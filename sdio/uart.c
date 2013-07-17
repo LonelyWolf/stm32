02964bcf6f93824d8d0cc579e9c65ad24eb1a2e4
Dmitry Semernin <dsemernin@gmail.com>
2013-07-17 09:17:50 +0300

    some UART lib optimizations

--- sdio/uart.c
+++ sdio/uart.c
@@ -37,27 +37,20 @@ void UART_SendChar(char ch) {
 }
 
 void UART_SendHex8(uint16_t num) {
-	UART_SendChar("0123456789ABCDEF"[(num >> 4)   % 0x10]);
-	UART_SendChar("0123456789ABCDEF"[(num & 0x0f) % 0x10]);
+	UART_SendChar(HEX_CHARS[(num >> 4)   % 0x10]);
+	UART_SendChar(HEX_CHARS[(num & 0x0f) % 0x10]);
 }
 
 void UART_SendHex16(uint16_t num) {
-	UART_SendChar("0123456789ABCDEF"[(num >> 12)  % 0x10]);
-	UART_SendChar("0123456789ABCDEF"[(num >> 8)   % 0x10]);
-	UART_SendChar("0123456789ABCDEF"[(num >> 4)   % 0x10]);
-	UART_SendChar("0123456789ABCDEF"[(num & 0x0f) % 0x10]);
+	uint8_t i;
+	for (i = 12; i > 0; i -= 4) UART_SendChar(HEX_CHARS[(num >> i) % 0x10]);
+	UART_SendChar(HEX_CHARS[(num & 0x0f) % 0x10]);
 }
 
 void UART_SendHex32(uint32_t num) {
-	// Ugly but less code and faster
-	UART_SendChar("0123456789ABCDEF"[(num >> 28)  % 0x10]);
-	UART_SendChar("0123456789ABCDEF"[(num >> 24)  % 0x10]);
-	UART_SendChar("0123456789ABCDEF"[(num >> 20)  % 0x10]);
-	UART_SendChar("0123456789ABCDEF"[(num >> 16)  % 0x10]);
-	UART_SendChar("0123456789ABCDEF"[(num >> 12)  % 0x10]);
-	UART_SendChar("0123456789ABCDEF"[(num >> 8)   % 0x10]);
-	UART_SendChar("0123456789ABCDEF"[(num >> 4)   % 0x10]);
-	UART_SendChar("0123456789ABCDEF"[(num & 0x0f) % 0x10]);
+	uint8_t i;
+	for (i = 28; i > 0; i -= 4)	UART_SendChar(HEX_CHARS[(num >> i) % 0x10]);
+	UART_SendChar(HEX_CHARS[(num & 0x0f) % 0x10]);
 }
 
 void UART_SendStr(char *str) {
@@ -66,10 +59,15 @@ void UART_SendStr(char *str) {
 
 void UART_SendBuf(char *buf, uint16_t bufsize) {
 	uint16_t i;
+	for (i = 0; i < bufsize; i++) UART_SendChar(*buf++);
+}
+
+void UART_SendBufPrintable(char *buf, uint16_t bufsize, char subst) {
+	uint16_t i;
 	char ch;
 	for (i = 0; i < bufsize; i++) {
 		ch = *buf++;
-		UART_SendChar(ch > 32 ? ch : '.');
+		UART_SendChar(ch > 32 ? ch : subst);
 	}
 }
 
@@ -78,13 +76,13 @@ void UART_SendBufHex(char *buf, uint16_t bufsize) {
 	char ch;
 	for (i = 0; i < bufsize; i++) {
 		ch = *buf++;
-		UART_SendChar("0123456789ABCDEF"[(ch >> 4)   % 0x10]);
-		UART_SendChar("0123456789ABCDEF"[(ch & 0x0f) % 0x10]);
+		UART_SendChar(HEX_CHARS[(ch >> 4)   % 0x10]);
+		UART_SendChar(HEX_CHARS[(ch & 0x0f) % 0x10]);
 	}
 }
 
-void UART_SendBufHexFancy(char *buf, uint16_t bufsize, uint8_t column_width) {
-	uint16_t i = 0,l,pos;
+void UART_SendBufHexFancy(char *buf, uint16_t bufsize, uint8_t column_width, char subst) {
+	uint16_t i = 0,len,pos;
 	char buffer[column_width];
 
 	while (i < bufsize) {
@@ -93,22 +91,19 @@ void UART_SendBufHexFancy(char *buf, uint16_t bufsize, uint8_t column_width) {
 		UART_SendChar(':'); UART_SendChar(' '); // Faster and less code than USART_SendStr(": ");
 
 		// Copy one line
-		if (i+column_width >= bufsize) l = bufsize - i; else l = column_width;
-		memcpy(buffer,&buf[i],l);
+		if (i+column_width >= bufsize) len = bufsize - i; else len = column_width;
+		memcpy(buffer,&buf[i],len);
 
 		// Hex data
 		pos = 0;
-		while (pos < l) UART_SendHex8(buffer[pos++]);
+		while (pos < len) UART_SendHex8(buffer[pos++]);
 		UART_SendChar(' ');
 
 		// Raw data
 		pos = 0;
-		while (pos < l) {
-			UART_SendChar(buffer[pos] > 32 ? buffer[pos] : '.');
-			pos++;
-		}
+		do UART_SendChar(buffer[pos] > 32 ? buffer[pos] : subst); while (++pos < len);
 		UART_SendChar('\n');
 
-		i += l;
+		i += len;
 	}
 }
--- sdio/uart.h
+++ sdio/uart.h
@@ -1,3 +1,5 @@
+#define HEX_CHARS      "0123456789ABCDEF"
+
 void UART_Init(void);
 void UART_SendChar(char ch);
 void UART_SendHex8(uint16_t num);
@@ -5,5 +7,6 @@ void UART_SendHex16(uint16_t num);
 void UART_SendHex32(uint32_t num);
 void UART_SendStr(char *str);
 void UART_SendBuf(char *buf, uint16_t bufsize);
+void UART_SendBufPrintable(char *buf, uint16_t bufsize, char subst);
 void UART_SendBufHex(char *buf, uint16_t bufsize);
-void UART_SendBufHexFancy(char *buf, uint16_t bufsize, uint8_t column_width);
+void UART_SendBufHexFancy(char *buf, uint16_t bufsize, uint8_t column_width, char subst);
