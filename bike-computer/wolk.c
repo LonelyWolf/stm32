c5bf7c7c321160a89733221ff311f5d1541fce98
Dmitry Semernin <dsemernin@gmail.com>
2014-06-24 00:20:31 +0300

    Settings stored in EEPROM, added stringlen() and numlen()

--- bike-computer/wolk.c
+++ bike-computer/wolk.c
@@ -1,13 +1,24 @@
 #include <stm32l1xx_rcc.h>
 #include <stm32l1xx_rtc.h>
+
 #include <wolk.h>
-
-nRF24_Packet_TypeDef nRF24_Packet;
-Cur_Data_TypeDef CurData;
+#include <EEPROM.h>
 
 
-uint16_t atos_len(uint8_t *buf, uint8_t len) {
-	uint16_t value = 0;
+nRF24_Packet_TypeDef nRF24_Packet;          // nRF24L01 last received packet
+Cur_Data_TypeDef CurData;                   // Current data (Speed, Cadence, etc.)
+BTN_TypeDef BTN[4];                         // Buttons
+Settings_TypeDef Settings;                  // Settings which stored in EEPROM
+
+
+// Convert characters to number with fixed length
+// input:
+//   buf - point to character buffer
+//   len - length of number to convert
+// return: converted number
+// note: only positive values
+uint32_t atos_len(uint8_t *buf, uint8_t len) {
+	uint32_t value = 0;
 	uint8_t digit;
 	uint8_t i;
 	char c;
@@ -21,6 +32,13 @@ uint16_t atos_len(uint8_t *buf, uint8_t len) {
     return value;
 }
 
+// Convert characters to number
+// from specified position until first non digital character
+// input:
+//   buf - point to character buffer
+//   pos - point to position inside buffer
+// return: converted number
+// note: pos value will be changed to the next character after converted number
 int32_t atos_char(uint8_t *buf, uint16_t *pos) {
 	uint32_t value = 0;
 	uint8_t digit;
@@ -43,4 +61,56 @@ int32_t atos_char(uint8_t *buf, uint16_t *pos) {
 	*pos += i + 1;
 
     return value * neg;
+}
+
+// BSD implementation of strlen
+// input:
+//   str - pointer to string
+// return: string length
+uint32_t stringlen(const char *str) {
+	const char *s;
+
+	for (s = str; *s; ++s);
+
+	return (s - str);
+}
+
+// Compute character length of numeric value
+// input:
+//   num - numeric value
+// return: length in characters (3456 -> 4)
+uint8_t numlen(int32_t num) {
+	uint8_t len = 1;
+
+	if (num < 0) {
+		num *= -1;
+		len++;
+	}
+	while ((num /= 10) > 0) len++;
+
+	return len;
+}
+
+// Load settings from EEPROM
+void ReadSettings_EEPROM(void) {
+	Settings_TypeDef settings;
+	uint32_t *ptr = (uint32_t *)&settings;
+	uint8_t i;
+
+	for (i = 0; i < sizeof(settings); i += 4) *ptr++ = EEPROM_Read(DATA_EEPROM_START_ADDR + i);
+}
+
+// Save settings to EEPROM
+void SaveSettings_EEPROM(void) {
+	Settings_TypeDef settings;
+	uint32_t *ptr = (uint32_t *)&settings;
+	uint8_t i;
+	uint32_t data;
+
+	EEPROM_Unlock();
+	for (i = 0; i < sizeof(settings); i += 4) {
+		data = *ptr++;
+		if (data != EEPROM_Read(DATA_EEPROM_START_ADDR + i)) EEPROM_Write(DATA_EEPROM_START_ADDR + i,data);
+	}
+	EEPROM_Lock();
 }
--- bike-computer/wolk.h
+++ bike-computer/wolk.h
@@ -10,6 +10,17 @@
 #define nRF24_RX_PAYLOAD                 17 // nRF24L01 payload length
 
 
+// Buttons
+#define      BTN1_PORT   GPIOA
+#define      BTN1_PIN    GPIO_Pin_5
+#define      BTN2_PORT   GPIOA
+#define      BTN2_PIN    GPIO_Pin_7
+#define      BTN3_PORT   GPIOC
+#define      BTN3_PIN    GPIO_Pin_10
+#define      BTN4_PORT   GPIOC
+#define      BTN4_PIN    GPIO_Pin_11
+
+
 // Just boolean
 typedef enum {
 	FALSE = 0,
@@ -23,7 +34,7 @@ typedef enum {
 	BTN_Hold     = 2
 } BTN_StateTypeDef;
 
-// Struct for data packet from sensor
+// Structure for data packet received from sensor
 typedef struct {
 	uint16_t cntr_SPD;         // SPD impulses counter
 	uint16_t tim_CDC;          // CDC interval
@@ -32,11 +43,10 @@ typedef struct {
 	uint8_t  observe_TX;       // Previous value of the OBSERVE_TX register
 	uint16_t cntr_wake;        // Wakeups counter
 	uint16_t packets_lost;     // Lost packets counter
-	uint8_t  tx_power;         // TX power
 	uint16_t ride_time;        // SPD interval from last delivered packet
 } nRF24_Packet_TypeDef;
 
-// Struct for current data
+// Structure for current data
 typedef struct {
 	uint16_t Speed;            // Current speed (km/h * 10)
 	uint16_t MaxSpeed;         // Trip maximum speed (km/h * 10)
@@ -53,9 +63,9 @@ typedef struct {
 	int32_t  Pressure;         // Current air pressure (Pa)
 	int32_t  MaxPressure;      // Maximum air pressure (Pa)
 	int32_t  MinPressure;      // Minimum air pressure (Pa)
-	int32_t  Altitude;         // Current altitude (m)
-	int32_t  MaxAltitude;      // Maximum altitude (m)
-	int32_t  MinAltitude;      // Minimum altitude (m)
+	int16_t  Altitude;         // Current altitude (m)
+	int16_t  MaxAltitude;      // Maximum altitude (m)
+	int16_t  MinAltitude;      // Minimum altitude (m)
 	int32_t  GPSAlt;           // Current GPS altitude (m)
 	int32_t  MaxGPSAlt;        // Maximum GPS altitude (m)
 	int32_t  MinGPSAlt;        // Minimum GPS altitude (m)
@@ -63,6 +73,7 @@ typedef struct {
 	uint32_t MaxGPSSpeed;      // Maximum GPS speed (km/h * 100)
 } Cur_Data_TypeDef;
 
+// Button structure
 typedef struct {
 	GPIO_TypeDef *PORT;       // Button GPIO port
 	uint16_t PIN;             // Button pin
@@ -71,21 +82,33 @@ typedef struct {
 	uint8_t hold_cntr;        // Button pressed counter (how long it's pushed)
 } BTN_TypeDef;
 
-
-static char * const nRF24_TX_POWERS[] = {"-18dBm","-12dBm","-6dBm","0dBm"};
+// Parameters which stored in EEPROM (structure must be aligned to 4 byte)
+typedef struct {
+	int16_t  altitude_home;       // Home altitude (m)
+	uint8_t  GMT_offset;          // GMT time offset (hours)
+	uint8_t  LCD_brightness;      // LCD brightness (percents 0..100)
+	uint16_t WheelCircumference;  // Wheel circumference (cm)
+} Settings_TypeDef;
 
 
 extern nRF24_Packet_TypeDef nRF24_Packet;          // nRF24L01 last received packet
 extern Cur_Data_TypeDef CurData;                   // Current data (Speed, Cadence, etc.)
+extern BTN_TypeDef BTN[4];                         // Buttons
+extern Settings_TypeDef Settings;                  // Settings which stored in EEPROM
 
 
 // Embedded internal reference voltage calibration value
-// Factory measured at Vdda = 3V (+/- 5mV)
+// Factory measured Vrefint at Vdda = 3V (+/- 5mV)
 #define VREFINT_CAL           ((uint16_t *)(uint32_t)0x1ff80078)
 
 
 // Function prototypes
-uint16_t atos_len(uint8_t *buf, uint8_t len);
+uint32_t atos_len(uint8_t *buf, uint8_t len);
 int32_t atos_char(uint8_t *buf, uint16_t *pos);
+uint32_t stringlen(const char *str);
+uint8_t numlen(int32_t num);
+
+void ReadSettings_EEPROM(void);
+void SaveSettings_EEPROM(void);
 
 #endif // __WOLK_H
