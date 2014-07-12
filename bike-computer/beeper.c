c34a7f2e003771a9d66c3083d7a60806dcc06fb3
Dmitry Semernin <dsemernin@gmail.com>
2014-07-13 00:23:19 +0300

    Function to play tone sequences

--- bike-computer/beeper.c
+++ bike-computer/beeper.c
@@ -2,21 +2,48 @@
 #include <stm32l1xx_gpio.h>
 #include <misc.h>
 
+#include <wolk.h> // bool typedef
 #include <beeper.h>
 
 
-volatile uint32_t _beep_duration;
-
+volatile       uint32_t          _beep_duration;
+volatile       bool              _tones_playing;
+volatile const Tone_TypeDef     *_tones;
 
 GPIO_InitTypeDef PORT;
 
 
 // BEEPER TIMx IRQ handler
 void TIM3_IRQHandler(void) {
-	TIM3->SR &= ~TIM_SR_UIF; // Clear the TIM3's interrupt pending bit
+	if (BEEPER_TIM->SR & TIM_SR_UIF) {
+		BEEPER_TIM->SR &= ~TIM_SR_UIF; // Clear the TIMx's interrupt pending bit
 
-	_beep_duration--;
-	if (_beep_duration == 0) BEEPER_Disable();
+		_beep_duration--;
+		if (_beep_duration == 0) {
+			if (_tones_playing) {
+				// Currently playing tones, take next tone
+				_tones++;
+				if (_tones->frequency == 0 && _tones->duration == 0) {
+					// Last tone in sequence
+					BEEPER_Disable();
+					_tones_playing = FALSE;
+					_tones = NULL;
+				} else {
+					if (_tones->frequency == 0) {
+						// Silence period
+						BEEPER_TIM->ARR = SystemCoreClock / (100 * BEEPER_TIM->PSC) - 1;
+						BEEPER_TIM->CCR2 = 0; // 0% duty cycle
+						_beep_duration = _tones->duration + 1;
+					} else {
+						// Play next tone in sequence
+						BEEPER_Enable(_tones->frequency,_tones->duration);
+					}
+				}
+			} else {
+				BEEPER_Disable();
+			}
+		}
+	}
 }
 
 // Initialize buzzer output
@@ -90,4 +117,11 @@ void BEEPER_Disable(void) {
 	PORT.GPIO_Mode = GPIO_Mode_AN;
 	PORT.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_Init(BEEPER_GPIO,&PORT);
+}
+
+// Play RTTTL tones
+void BEEPER_PlayTones(const Tone_TypeDef * tones) {
+	_tones = tones;
+	_tones_playing = TRUE;
+	BEEPER_Enable(_tones->frequency,_tones->duration);
 }
