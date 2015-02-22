b10af7a327b3a3ba99155872a5bfee2c02f086a8
Dmitry Semernin <dsemernin@gmail.com>
2015-02-22 02:02:16 +0200

    Small updates to the SPI library

--- stm32l151rdt6-dev/spi.c
+++ stm32l151rdt6-dev/spi.c
@@ -1,3 +1,4 @@
+
 #include <stm32l1xx_gpio.h>
 #include <stm32l1xx_rcc.h>
 
@@ -7,7 +8,9 @@
 // SPI peripheral initialization
 // input:
 //   SPI - pointer to the SPI port (SPI1, SPI2, etc.)
-void SPIx_Init(SPI_TypeDef *SPI) {
+//   SPI_direction - SPI lines configuration (one of SPI_DIR_XXX values)
+//   SPI_prescaler - SPI prescaler (SPI_BR_XXX)
+void SPIx_Init(SPI_TypeDef *SPI, uint16_t SPI_direction, uint16_t SPI_prescaler) {
 	GPIO_InitTypeDef PORT;
 
 	PORT.GPIO_Speed = GPIO_Speed_40MHz;
@@ -16,46 +19,115 @@ void SPIx_Init(SPI_TypeDef *SPI) {
 	PORT.GPIO_PuPd  = GPIO_PuPd_NOPULL;
 
 	if (SPI == SPI1) {
-		RCC_AHBPeriphClockCmd(SPI1_PERIPH,ENABLE); // Enable the SPI GPIO peripheral
-		RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1,ENABLE); // Enable the SPI peripheral
-		PORT.GPIO_Pin = SPI1_SCK_PIN | SPI1_MOSI_PIN | SPI1_MISO_PIN;
-		GPIO_Init(SPI1_GPIO_PORT,&PORT);
-		GPIO_PinAFConfig(SPI1_GPIO_PORT,SPI1_SCK_PIN_SRC, GPIO_AF_SPI1);
-		GPIO_PinAFConfig(SPI1_GPIO_PORT,SPI1_MISO_PIN_SRC,GPIO_AF_SPI1);
-		GPIO_PinAFConfig(SPI1_GPIO_PORT,SPI1_MOSI_PIN_SRC,GPIO_AF_SPI1);
-	} else {
-		RCC_AHBPeriphClockCmd(SPI2_PERIPH,ENABLE); // Enable the SPI GPIO peripheral
-		RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI2,ENABLE); // Enable the SPI peripheral
-		PORT.GPIO_Pin = SPI2_SCK_PIN | SPI2_MOSI_PIN | SPI2_MISO_PIN;
-		GPIO_Init(SPI2_GPIO_PORT,&PORT);
-		GPIO_PinAFConfig(SPI2_GPIO_PORT,SPI2_SCK_PIN_SRC, GPIO_AF_SPI2);
-		GPIO_PinAFConfig(SPI2_GPIO_PORT,SPI2_MISO_PIN_SRC,GPIO_AF_SPI2);
-		GPIO_PinAFConfig(SPI2_GPIO_PORT,SPI2_MOSI_PIN_SRC,GPIO_AF_SPI2);
+		// Reset the SPI1 peripheral
+		RCC->APB2RSTR |=  RCC_APB2RSTR_SPI1RST;
+		RCC->APB2RSTR &= ~RCC_APB2RSTR_SPI1RST;
+		// Enable the SPI GPIO peripheral(s)
+		RCC->AHBENR |= SPI1_PERIPH;
+		// Enable the SPI1 peripheral
+		RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
+		// SCK pin
+		PORT.GPIO_Pin = SPI1_SCK_PIN;
+		GPIO_Init(SPI1_SCK_GPIO,&PORT);
+		GPIO_PinAFConfig(SPI1_SCK_GPIO,SPI1_SCK_PIN_SRC,GPIO_AF_SPI1);
+		if (SPI_direction != SPI_DIR_RX) {
+			// MOSI pin
+			PORT.GPIO_Pin = SPI1_MOSI_PIN;
+			GPIO_Init(SPI1_MOSI_GPIO,&PORT);
+			GPIO_PinAFConfig(SPI1_MOSI_GPIO,SPI1_MOSI_PIN_SRC,GPIO_AF_SPI1);
+		}
+		if (SPI_direction != SPI_DIR_TX) {
+			// MISO pin
+			PORT.GPIO_Pin = SPI1_MISO_PIN;
+			GPIO_Init(SPI1_MISO_GPIO,&PORT);
+			GPIO_PinAFConfig(SPI1_MISO_GPIO,SPI1_MISO_PIN_SRC,GPIO_AF_SPI1);
+		}
+	} else if (SPI == SPI2) {
+		// Reset the SPI2 peripheral
+		RCC->APB1RSTR |=  RCC_APB1RSTR_SPI2RST;
+		RCC->APB1RSTR &= ~RCC_APB1RSTR_SPI2RST;
+		// Enable the SPI2 GPIO peripheral
+		RCC->AHBENR |= SPI2_PERIPH;
+		// Enable the SPI2 peripheral
+		RCC->APB1ENR |= RCC_APB1ENR_SPI2EN;
+		// SCK pin
+		PORT.GPIO_Pin = SPI2_SCK_PIN;
+		GPIO_Init(SPI2_SCK_GPIO,&PORT);
+		GPIO_PinAFConfig(SPI2_SCK_GPIO,SPI2_SCK_PIN_SRC,GPIO_AF_SPI2);
+		if (SPI_direction != SPI_DIR_RX) {
+			// MOSI pin
+			PORT.GPIO_Pin = SPI2_MOSI_PIN;
+			GPIO_Init(SPI2_MOSI_GPIO,&PORT);
+			GPIO_PinAFConfig(SPI2_MOSI_GPIO,SPI2_MOSI_PIN_SRC,GPIO_AF_SPI2);
+		}
+		if (SPI_direction != SPI_DIR_TX) {
+			// MISO pin
+			PORT.GPIO_Pin = SPI2_MISO_PIN;
+			GPIO_Init(SPI2_MISO_GPIO,&PORT);
+			GPIO_PinAFConfig(SPI2_MISO_GPIO,SPI2_MISO_PIN_SRC,GPIO_AF_SPI2);
+		}
+	} else if (SPI == SPI3) {
+		// Reset the SPI3 peripheral
+		RCC->APB1RSTR |=  RCC_APB1RSTR_SPI3RST;
+		RCC->APB1RSTR &= ~RCC_APB1RSTR_SPI3RST;
+		// Enable the SPI3 GPIO peripheral(s)
+		RCC->AHBENR |= SPI3_PERIPH;
+		// Enable the SPI3 peripheral
+		RCC->APB1ENR |= RCC_APB1ENR_SPI3EN;
+		// SCK pin
+		PORT.GPIO_Pin = SPI3_SCK_PIN;
+		GPIO_Init(SPI3_SCK_GPIO,&PORT);
+		GPIO_PinAFConfig(SPI3_SCK_GPIO,SPI3_SCK_PIN_SRC,GPIO_AF_SPI3);
+		if (SPI_direction != SPI_DIR_RX) {
+			// MOSI pin
+			PORT.GPIO_Pin = SPI3_MOSI_PIN;
+			GPIO_Init(SPI3_MOSI_GPIO,&PORT);
+			GPIO_PinAFConfig(SPI3_MOSI_GPIO,SPI3_MOSI_PIN_SRC,GPIO_AF_SPI3);
+		}
+		if (SPI_direction != SPI_DIR_TX) {
+			// MISO pin
+			PORT.GPIO_Pin = SPI3_MISO_PIN;
+			GPIO_Init(SPI3_MISO_GPIO,&PORT);
+			GPIO_PinAFConfig(SPI3_MISO_GPIO,SPI3_MISO_PIN_SRC,GPIO_AF_SPI3);
+		}
 	}
 
-	SPI->CR1 &= 0x3040; // Clear SPI settings
 	// SPI settings:
 	//   - master mode
-	//   - 2 lines full duplex
 	//   - CPOL low
 	//   - CPHA 1 edge
 	//   - data size 8 bit
 	//   - MSB first bit
 	//   - software NSS selection
-	//   - baudrate prescaler 2
-	SPI->CR1 |= SPI_CR1_MSTR | SPI_CR1_SSI | SPI_CR1_SSM; // Master mode
+	//   - hardware CRC calculation disabled
+	//   - peripheral enabled
+	SPI->CR1 = SPI_CR1_MSTR | SPI_CR1_SSI | SPI_CR1_SSM | SPI_prescaler;
+	if (SPI_direction == SPI_DIR_RX) SPI->CR1 |= SPI_CR1_RXONLY;
 	SPI->I2SCFGR &= ~SPI_I2SCFGR_I2SMOD; // Clear I2SMOD bit - SPI mode
-	SPI->CRCPR = 7; // Polynomial for CRC calculation
+	SPI->CRCPR = 7; // Polynomial for CRC calculation (must be set when the SPE bit cleared in the CR1 register)
 	SPI->CR1 |= SPI_CR1_SPE; // Enable the specified SPI peripheral
 }
 
 // Set SPI speed
 // input:
 //   SPI - pointer to the SPI port (SPI1, SPI2, etc.)
-//   prescaler - SPI prescaler (SPI_BR_x)
-void SPIx_SetSpeed(SPI_TypeDef *SPI, uint16_t prescaler) {
-	SPI->CR1 &= ~SPI_CR1_BR; // Clear SPI baud rate bits
-	SPI->CR1 |= prescaler; // Set SPI baud rate bits
+//   SPI_prescaler - SPI prescaler (SPI_BR_XXX)
+void SPIx_SetSpeed(SPI_TypeDef *SPI, uint16_t SPI_prescaler) {
+	uint16_t reg;
+
+	reg  = SPI->CR1 & ~SPI_CR1_BR; // Clear SPI baud rate bits
+	reg |= SPI_prescaler; // Set SPI baud rate bits
+	SPI->CR1 = reg; // Update the SPI configuration
+}
+
+// Send data via SPI
+// input:
+//   SPI - pointer to the SPI port (SPI1, SPI2, etc.)
+//   data - byte to send
+void SPIx_Send(SPI_TypeDef *SPI, uint8_t data) {
+	SPI->DR = data; // Send byte to SPI (TXE cleared)
+	while (!(SPI->SR & SPI_SR_RXNE)); // Wait while receive buffer is empty
+	(void)SPI->DR; // Clear the RXNE bit
 }
 
 // Send/Receive data via SPI
@@ -64,8 +136,7 @@ void SPIx_SetSpeed(SPI_TypeDef *SPI, uint16_t prescaler) {
 //   data - byte to send
 // return: byte received by SPI
 uint8_t SPIx_SendRecv(SPI_TypeDef *SPI, uint8_t data) {
-	while (!(SPI->SR & SPI_SR_TXE)); // Wait while receive buffer is not empty
-	SPI->DR = data; // Send byte to SPI
+	SPI->DR = data; // Send byte to SPI (TXE cleared)
 	while (!(SPI->SR & SPI_SR_RXNE)); // Wait while receive buffer is empty
 
 	return SPI->DR; // Received byte
--- stm32l151rdt6-dev/spi.h
+++ stm32l151rdt6-dev/spi.h
@@ -3,40 +3,72 @@
 #define __SPIx_H
 
 // SPI1
-#define SPI1_PORT          SPI1
-#define SPI1_PERIPH        RCC_AHBPeriph_GPIOB
-#define SPI1_SCK_PIN       GPIO_Pin_3    // PB3
-#define SPI1_MISO_PIN      GPIO_Pin_4    // PB4
-#define SPI1_MOSI_PIN      GPIO_Pin_5    // PB5
-#define SPI1_SCK_PIN_SRC   GPIO_PinSource3
-#define SPI1_MISO_PIN_SRC  GPIO_PinSource4
-#define SPI1_MOSI_PIN_SRC  GPIO_PinSource5
-#define SPI1_GPIO_PORT     GPIOB
+// SCK : PA5, PB3
+// MISO: PA6, PB4, PA11
+// MOSI: PA7, PB5, PA12
+#define SPI1_PORT           SPI1
+#define SPI1_PERIPH         RCC_AHBENR_GPIOAEN
+#define SPI1_SCK_GPIO       GPIOA
+#define SPI1_SCK_PIN        GPIO_Pin_5
+#define SPI1_SCK_PIN_SRC    GPIO_PinSource5
+#define SPI1_MISO_GPIO      GPIOA
+#define SPI1_MISO_PIN       GPIO_Pin_6
+#define SPI1_MISO_PIN_SRC   GPIO_PinSource6
+#define SPI1_MOSI_GPIO      GPIOA
+#define SPI1_MOSI_PIN       GPIO_Pin_7
+#define SPI1_MOSI_PIN_SRC   GPIO_PinSource7
 
 // SPI2
-#define SPI2_PORT          SPI2
-#define SPI2_PERIPH        RCC_AHBPeriph_GPIOB
-#define SPI2_SCK_PIN       GPIO_Pin_13    // PB13
-#define SPI2_MISO_PIN      GPIO_Pin_14    // PB14
-#define SPI2_MOSI_PIN      GPIO_Pin_15    // PB15
-#define SPI2_SCK_PIN_SRC   GPIO_PinSource13
-#define SPI2_MISO_PIN_SRC  GPIO_PinSource14
-#define SPI2_MOSI_PIN_SRC  GPIO_PinSource15
-#define SPI2_GPIO_PORT     GPIOB
+// SCK : PB13
+// MISO: PB14
+// MOSI: PB15
+#define SPI2_PORT           SPI2
+#define SPI2_PERIPH         RCC_AHBENR_GPIOBEN
+#define SPI2_SCK_GPIO       GPIOB
+#define SPI2_SCK_PIN        GPIO_Pin_13
+#define SPI2_SCK_PIN_SRC    GPIO_PinSource13
+#define SPI2_MISO_GPIO      GPIOB
+#define SPI2_MISO_PIN       GPIO_Pin_14
+#define SPI2_MISO_PIN_SRC   GPIO_PinSource14
+#define SPI2_MOSI_GPIO      GPIOB
+#define SPI2_MOSI_PIN       GPIO_Pin_15
+#define SPI2_MOSI_PIN_SRC   GPIO_PinSource15
+
+// SPI3
+// SCK : PC10, PB3
+// MISO: PB11, PB4
+// MOSI: PB12, PB5
+#define SPI3_PORT           SPI3
+#define SPI3_PERIPH         RCC_AHBENR_GPIOCEN
+#define SPI3_SCK_GPIO       GPIOC
+#define SPI3_SCK_PIN        GPIO_Pin_10
+#define SPI3_SCK_PIN_SRC    GPIO_PinSource10
+#define SPI3_MISO_GPIO      GPIOC
+#define SPI3_MISO_PIN       GPIO_Pin_11
+#define SPI3_MISO_PIN_SRC   GPIO_PinSource11
+#define SPI3_MOSI_GPIO      GPIOC
+#define SPI3_MOSI_PIN       GPIO_Pin_12
+#define SPI3_MOSI_PIN_SRC   GPIO_PinSource12
+
+// SPI baud rate prescaler
+#define SPI_BR_2           ((uint16_t)0x0000) // 2
+#define SPI_BR_4           ((uint16_t)0x0008) // 4
+#define SPI_BR_8           ((uint16_t)0x0010) // 8
+#define SPI_BR_16          ((uint16_t)0x0018) // 16
+#define SPI_BR_32          ((uint16_t)0x0020) // 32
+#define SPI_BR_64          ((uint16_t)0x0028) // 64
+#define SPI_BR_128         ((uint16_t)0x0030) // 128
+#define SPI_BR_256         ((uint16_t)0x0038) // 256
+
+// SPI line configuration
+#define SPI_DIR_DUPLEX     ((uint8_t)0x00) // 2 lines full duplex
+#define SPI_DIR_RX         ((uint8_t)0x01) // 1 line RX
+#define SPI_DIR_TX         ((uint8_t)0x02) // 1 line TX
 
 
-#define SPI_BR_2           ((uint16_t)0x0000)    // SPI baud rate prescaler 2
-#define SPI_BR_4           ((uint16_t)0x0008)    // SPI baud rate prescaler 4
-#define SPI_BR_8           ((uint16_t)0x0010)    // SPI baud rate prescaler 8
-#define SPI_BR_16          ((uint16_t)0x0018)    // SPI baud rate prescaler 16
-#define SPI_BR_32          ((uint16_t)0x0020)    // SPI baud rate prescaler 32
-#define SPI_BR_64          ((uint16_t)0x0028)    // SPI baud rate prescaler 64
-#define SPI_BR_128         ((uint16_t)0x0030)    // SPI baud rate prescaler 128
-#define SPI_BR_256         ((uint16_t)0x0038)    // SPI baud rate prescaler 256
-
-
-void SPIx_Init(SPI_TypeDef *SPI);
-void SPIx_SetSpeed(SPI_TypeDef *SPI, uint16_t prescaler);
+void SPIx_Init(SPI_TypeDef *SPI, uint16_t SPI_direction, uint16_t SPI_prescaler);
+void SPIx_SetSpeed(SPI_TypeDef *SPI, uint16_t SPI_prescaler);
+void SPIx_Send(SPI_TypeDef *SPI, uint8_t data);
 uint8_t SPIx_SendRecv(SPI_TypeDef *SPI, uint8_t data);
 
 #endif // __SPIx_H
