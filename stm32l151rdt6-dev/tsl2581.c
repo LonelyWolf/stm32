872426714ef7493fd014fb96178428e75a211f33
Dmitry Semernin <dsemernin@gmail.com>
2015-04-28 22:53:25 +0300

    TSL2581: Lux calculation (by datasheet)

--- stm32l151rdt6-dev/tsl2581.c
+++ stm32l151rdt6-dev/tsl2581.c
@@ -4,6 +4,10 @@
 #include "tsl2581.h"
 
 
+uint8_t TSL2581_gain = TSL2581_GAIN1; // Current gain value
+uint8_t TSL2581_int_cycles = TSL2581_NOM_INTEG_CYCLE; // Current integration cycles value
+
+
 // Write new value to the TSL2581 register
 // input:
 //   addr - register address
@@ -54,11 +58,11 @@ uint16_t TSL2581_ReadReg16(uint8_t addr) {
 void TSL2581_Init(void) {
 	// Turn on chip power
 	TSL2581_PowerOn();
-	// Set 99.9ms integration time (37 * 2.7ms)
-	TSL2581_SetTime(37);
-	// Enable ADC (by datasheet a 2ms delay mandatory before enabling ADC)
+	// Set 400ms integration time (148 * 2.7ms)
+	TSL2581_SetTime(TSL2581_NOM_INTEG_CYCLE);
+	// Enable ADC (by datasheet a 2ms delay is mandatory before enabling ADC)
 	TSL2581_ADCOn();
-	// Set analog gain 1x
+	// Set analog gain x1
 	TSL2581_SetGain(TSL2581_GAIN1);
 }
 
@@ -84,12 +88,9 @@ void TSL2581_ADCOn(void) {
 	TSL2581_WriteReg(TSL2581_CONTROL,TSL2581_CTL_POWER | TSL2581_CTL_ADC);
 }
 
-// Set integration time of the ADC channels
-// input:
-//   time - time in 2.7-ms intervals
-// note: time = 0 --> manual integration mode, 0xff = 2.7ms interval, 0x01 = 688.5ms
-void TSL2581_SetTime(uint8_t time) {
-	TSL2581_WriteReg(TSL2581_TIMING,time);
+// Disable the ADC
+void TSL2581_ADCOff(void) {
+	TSL2581_WriteReg(TSL2581_CONTROL,TSL2581_CTL_POWER & ~TSL2581_CTL_ADC);
 }
 
 // Read ADC channel 0 value (visible and IR)
@@ -104,12 +105,25 @@ uint16_t TSL2581_GetData1(void) {
 	return TSL2581_ReadReg16(TSL2581_DATA1L);
 }
 
+// Set integration time of the ADC channels
+// input:
+//   time - time in 2.7-ms intervals
+// note: time = 0 --> manual integration mode, 0xff = 2.7ms interval, 0x01 = 688.5ms
+void TSL2581_SetTime(uint8_t time) {
+	TSL2581_int_cycles = time;
+	TSL2581_WriteReg(TSL2581_TIMING,TSL2581_int_cycles);
+}
+
 // Set analog gain of the device
 // input:
 //   gain - TSL2581_GAINx values
 void TSL2581_SetGain(uint8_t gain) {
 	if (gain > TSL2581_GAIN111) gain = TSL2581_GAIN111;
-	TSL2581_WriteReg(TSL2581_ANALOG,gain);
+	TSL2581_gain = gain;
+	// Disable the ADC before setting a new gain value to prevent indeterminate result
+	TSL2581_ADCOff();
+	TSL2581_WriteReg(TSL2581_ANALOG,TSL2581_gain);
+	TSL2581_ADCOn();
 }
 
 // Read device ID
@@ -118,4 +132,84 @@ void TSL2581_SetGain(uint8_t gain) {
 //       [3..0] bits - revision number identification
 uint8_t TSL2581_GetDeviceID(void) {
 	return TSL2581_ReadReg(TSL2581_ID);
+}
+
+// From TAOS datasheet
+//////////////////////////////////////////////////////////////////////////////
+// lux equation approximation without floating point calculations
+// Description: Calculate the approximate illuminance (lux) given the raw
+// channel values of the TSL2583. The equation if implemented
+// as a piece−wise linear approximation.
+// input:
+//   tIntCycles − INTEG_CYCLES defined in Timing Register
+//   d0 − raw channel value from channel 0 of TSL2583
+//   d1 − raw channel value from channel 1 of TSL2583
+// return: the approximate illuminance (lux)
+// note: in case of sensor saturation (d0=d1=65535) function will return 0
+uint32_t TSL2581_LuxCalc(uint16_t d0, uint16_t d1) {
+	uint32_t chScale0;
+	uint32_t chScale1;
+	uint32_t channel1;
+	uint32_t channel0;
+	uint16_t b,m;
+	uint32_t lux;
+	uint32_t ratio = 0;
+
+	// First, scale the channel values depending on the gain and integration time (1X, 400ms is nominal setting)
+
+	// No scaling if nominal integration (148 cycles or 400 ms) is used
+	if (TSL2581_int_cycles == TSL2581_NOM_INTEG_CYCLE) {
+		chScale0 = (1 << TSL2581_CH_SCALE);
+	} else {
+		chScale0 = ((TSL2581_NOM_INTEG_CYCLE << TSL2581_CH_SCALE) / TSL2581_int_cycles);
+	}
+	switch (TSL2581_gain) {
+		case 0:
+			// 1x gain: no scale, nominal setting
+			chScale1 = chScale0; //
+			break;
+		case 1:
+			// 8x gain: scale/multiply value by 1/8
+			chScale0 = chScale0 >> 3;
+			chScale1 = chScale0;
+			break;
+		case 2: // 16x gain: scale/multiply value by 1/16
+			chScale0 = chScale0 >> 4;
+			chScale1 = chScale0;
+			break;
+		case 3: // 128x gain: CH0/CH1 gain correction factors applied
+			chScale1 = chScale0 / TSL2581_CH1GAIN128X;
+			chScale0 = chScale0 / TSL2581_CH0GAIN128X;
+		break;
+	}
+
+	// Scale the channel values
+	channel0 = (d0 * chScale0) >> TSL2581_CH_SCALE;
+	channel1 = (d1 * chScale1) >> TSL2581_CH_SCALE;
+
+	// Find the ratio of the channel values (Channel1/Channel0), protect against divide by zero
+	if (channel0) ratio = (((channel1 << (TSL2581_RATIO_SCALE + 1)) / channel0) + 1) >> 1;
+	if ((ratio >= 0) && (ratio <= TSL2581_K1C)) {
+		b = TSL2581_B1C;
+		m = TSL2581_M1C;
+	} else if (ratio <= TSL2581_K2C) {
+		b = TSL2581_B2C;
+		m = TSL2581_M2C;
+	} else if (ratio <= TSL2581_K3C) {
+		b = TSL2581_B3C;
+		m = TSL2581_M3C;
+	} else if (ratio <= TSL2581_K4C) {
+		b = TSL2581_B4C;
+		m = TSL2581_M4C;
+	} else if (ratio >  TSL2581_K5C) {
+		b = TSL2581_B5C;
+		m = TSL2581_M5C;
+	}
+
+	// Calculate a lux value
+	lux   = (channel0 * b) - (channel1 * m);
+	lux  += 1 << (TSL2581_LUX_SCALE - 1); // Round lsb (2^(LUX_SCALE − 1))
+	lux >>= TSL2581_LUX_SCALE; // Strip off fractional portion
+
+	return lux;
 }
--- stm32l151rdt6-dev/tsl2581.h
+++ stm32l151rdt6-dev/tsl2581.h
@@ -45,21 +45,61 @@
 
 // Analog gain control values
 enum {
-	TSL2581_GAIN1   = 0, // 1x
-	TSL2581_GAIN8   = 1, // 8x
-	TSL2581_GAIN16  = 2, // 16x
-	TSL2581_GAIN111 = 3  // 111x
+	TSL2581_GAIN1   = 0, // x1
+	TSL2581_GAIN8   = 1, // x8
+	TSL2581_GAIN16  = 2, // x16
+	TSL2581_GAIN111 = 3  // x111
 };
+
+
+// For Lux calculation (from TAOS datasheet)
+// Scale factors
+#define TSL2581_LUX_SCALE           16 // scale by 2^16
+#define TSL2581_RATIO_SCALE          9 // scale ratio by 2^9
+// Integration time scaling factors
+#define TSL2581_CH_SCALE            16 // scale channel values by 2^16
+#define TSL2581_NOM_INTEG_CYCLE    148 // Nominal 400 ms integration. See Timing Register
+// Gain scaling factors
+#define TSL2581_CH0GAIN128X        107 // 128X gain scalar for Ch0
+#define TSL2581_CH1GAIN128X        115 // 128X gain scalar for Ch1
+// FN Package coefficients
+// For Ch1/Ch0=0.00 to 0.30: Lux=0.130*Ch0−0.240*Ch1
+// For Ch1/Ch0=0.30 to 0.38: Lux=0.1649*Ch0−0.3562*Ch1
+// For Ch1/Ch0=0.38 to 0.45: Lux=0.0974*Ch0−0.1786*Ch1
+// For Ch1/Ch0=0.45 to 0.54: Lux=0.062*Ch0−0.10*Ch1
+// For Ch1/Ch0>0.54        : Lux/Ch0=0
+#define TSL2581_K1C             0x009A // 0.30 * 2^RATIO_SCALE
+#define TSL2581_B1C             0x2148 // 0.130 * 2^LUX_SCALE
+#define TSL2581_M1C             0x3d71 // 0.240 * 2^LUX_SCALE
+#define TSL2581_K2C             0x00c3 // 0.38 * 2^RATIO_SCALE
+#define TSL2581_B2C             0x2a37 // 0.1649 * 2^LUX_SCALE
+#define TSL2581_M2C             0x5b30 // 0.3562 * 2^LUX_SCALE
+#define TSL2581_K3C             0x00e6 // 0.45 * 2^RATIO_SCALE
+#define TSL2581_B3C             0x18ef // 0.0974 * 2^LUX_SCALE
+#define TSL2581_M3C             0x2db9 // 0.1786 * 2^LUX_SCALE
+#define TSL2581_K4C             0x0114 // 0.54 * 2^RATIO_SCALE
+#define TSL2581_B4C             0x0fdf // 0.062 * 2^LUX_SCALE
+#define TSL2581_M4C             0x199a // 0.10 * 2^LUX_SCALE
+#define TSL2581_K5C             0x0114 // 0.54 * 2^RATIO_SCALE
+#define TSL2581_B5C             0x0000 // 0.00000 * 2^LUX_SCALE
+#define TSL2581_M5C             0x0000 // 0.00000 * 2^LUX_SCALE
+
+
+// Public variables
+extern uint8_t TSL2581_gain; // Current gain value
+extern uint8_t TSL2581_int_cycles; // Current integration cycles value
 
 
 void TSL2581_Init(void);
 void TSL2581_PowerOn(void);
 void TSL2581_PowerOff(void);
 void TSL2581_ADCOn(void);
-void TSL2581_SetTime(uint8_t time);
+void TSL2581_ADCOff(void);
 uint16_t TSL2581_GetData0(void);
 uint16_t TSL2581_GetData1(void);
+void TSL2581_SetTime(uint8_t time);
 void TSL2581_SetGain(uint8_t gain);
 uint8_t TSL2581_GetDeviceID(void);
+uint32_t TSL2581_LuxCalc(uint16_t d0, uint16_t d1);
 
 #endif // __TSL2581_H
