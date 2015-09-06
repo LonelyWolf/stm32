c30e2e25168058ead7d5d3646eba255624cc7a0f
Dmitry Semernin <dsemernin@gmail.com>
2015-09-06 23:46:10 +0300

    Some polish to Delay lib

--- stm32l151rdt6-dev/delay.h
+++ stm32l151rdt6-dev/delay.h
@@ -1,18 +1,21 @@
-// Define to prevent recursive inclusion -------------------------------------
 #ifndef __DELAY_H
 #define __DELAY_H
 
 
+#include <stm32l1xx.h>
+
+
 // Delay timer HAL
 #define DELAY_TIM               TIM6
-#define DELAY_TIM_PERIPH        RCC_APB1Periph_TIM6
 #define DELAY_TIM_IRQN          TIM6_IRQn
 #define DELAY_TIM_APB           RCC->APB1ENR
+#define DELAY_TIM_PERIPH        RCC_APB1ENR_TIM6EN
 
 // NULL declaration
 #ifndef NULL
 #define NULL  ((void *)0)
 #endif
+
 
 // Callback function prototype
 typedef void (*funcCallback_TypeDef)(void);
@@ -24,6 +27,5 @@ void Delay_Disable(void);
 void Delay_Enable(void);
 
 void Delay_ms(uint32_t nTime);
-void Delay_us(uint32_t nTime);
 
 #endif // __DELAY_H
