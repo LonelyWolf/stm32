c14c008e5edc0f0a956a2608e246358b04c2eb48
Dmitry Semernin <dsemernin@gmail.com>
2016-11-03 23:41:24 +0200

    delay.c updated

--- nrf24l01/delay.c
+++ nrf24l01/delay.c
@@ -1,22 +1,37 @@
-#include <stm32f10x_rcc.h>
-#include <delay.h>
+// Delay functions using SysTick timer
+// Minimal delay length is 1ms
 
 
-// SysTick interrupt handler
-void SysTick_Handler() {
-	if (TimingDelay != 0) { TimingDelay--; }
+#include "delay.h"
+
+
+// Initialize delay (configure SysTick counter)
+// note: must be called each time when the system core clock has been changed
+void Delay_Init(void) {
+    // Set reload register to generate IRQ every millisecond
+    SysTick->LOAD = (uint32_t)((SystemCoreClock / 1000UL) - 1UL);
+
+    // Set priority for SysTick IRQ
+    NVIC_SetPriority(SysTick_IRQn,(1UL << __NVIC_PRIO_BITS) - 1UL);
+
+    // Set the SysTick counter value
+    SysTick->VAL = 0UL;
+
+    // Configure SysTick source and enable counter
+    SysTick->CTRL = (SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk);
 }
 
-// Do delay for mSecs milliseconds
-void Delay_ms(uint32_t mSecs) {
-	SysTick_Config(SystemCoreClock / DELAY_TICK_FREQUENCY_MS);
-	TimingDelay = mSecs+1;
-	while (TimingDelay != 0);
-}
+#if (!DELAY_INLINE)
+// Do a delay for a specified number of milliseconds
+// input:
+//   ms - number of milliseconds to wait
+void Delay_ms(uint32_t ms) {
+    __IO uint32_t delay_counter = ms;
 
-// Do delay for uSecs microseconds
-void Delay_us(uint32_t uSecs) {
-	SysTick_Config(SystemCoreClock / DELAY_TICK_FREQUENCY_US);
-	TimingDelay = uSecs+1;
-	while (TimingDelay != 0);
+    while (delay_counter) {
+        if (SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk) {
+            delay_counter--;
+        }
+    }
 }
+#endif
