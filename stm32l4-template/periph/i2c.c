5eb4b550b5324d282333658aa8d07d4878cab271
Dmitry Semernin <dsemernin@gmail.com>
2018-04-24 02:02:19 +0300

    stm32l4: i2c lib update

--- stm32l4-template/periph/i2c.c
+++ stm32l4-template/periph/i2c.c
@@ -5,7 +5,7 @@
 
 
 // I2C timeout, about 2ms
-#define I2C_TIMEOUT             100U
+#define I2C_TIMEOUT             200U
 
 // Maximum NBYTES value
 #define I2C_NBYTES_MAX          255U
@@ -91,8 +91,6 @@ void I2C_Init(I2C_TypeDef* I2Cx) {
 		GPIO_set_mode(I2C3_SDA_GPIO_PORT, GPIO_Mode_AF, GPIO_PUPD_NONE, I2C3_SDA_GPIO_PIN);
 		GPIO_af_cfg(I2C3_SDA_GPIO_PORT, I2C3_SDA_GPIO_SRC, GPIO_AF4); // AF4 = I2C3
 		GPIO_out_cfg(I2C3_SDA_GPIO_PORT, GPIO_OT_OD, GPIO_SPD_LOW, I2C3_SDA_GPIO_PIN);
-	} else {
-		// Unknown I2C port specified
 	}
 }
 
@@ -109,6 +107,9 @@ I2CSTATUS I2C_IsDeviceReady(I2C_TypeDef* I2Cx, uint8_t devAddr, uint32_t trials)
 	uint32_t reg;
 
 	while (trials--) {
+		// Clear all flags
+		I2Cx->ICR = I2C_ICR_ALL;
+
 		// Generate START
 		I2C_GenStart(I2Cx, devAddr);
 
@@ -127,7 +128,9 @@ I2CSTATUS I2C_IsDeviceReady(I2C_TypeDef* I2Cx, uint8_t devAddr, uint32_t trials)
 
 		// Check for BERR flag
 		if (reg & I2C_ISR_BERR) {
-			// FIXME: I2C: how to handle the misplaced START/STOP?
+			// Misplaced START/STOP? Perform a software reset of I2C
+			I2C_Disable(I2Cx);
+			I2C_Enable(I2Cx);
 		} else {
 			// Device responded if NACK flag is not set
 			if (!(reg & I2C_ISR_NACKF)) { return I2C_SUCCESS; }
@@ -143,44 +146,57 @@ I2CSTATUS I2C_IsDeviceReady(I2C_TypeDef* I2Cx, uint8_t devAddr, uint32_t trials)
 //   pBbuf - pointer to the data buffer
 //   nbytes - number of bytes to transmit
 //   devAddr - address of target device
-//   stop - generate or not the STOP condition (I2C_GENSTOP_YES or I2C_GENSTOP_NO)
+//   flags - options for transmission, combination of I2C_TX_xx values:
+//     I2C_TX_NOSTART - don't generate START condition
+//     I2C_TX_NOSTOP - don't generate STOP condition
+//     I2C_TX_CONT - this flag indicates that transmission will be continued
+//                   e.g. by calling this function again with NOSTART flag
+//     zero value - generate both START and STOP conditions
 // return:
 //   I2C_ERROR if there was a timeout during I2C operations, I2C_SUCCESS otherwise
-I2CSTATUS I2C_Transmit(I2C_TypeDef* I2Cx, const uint8_t *pBuf, uint32_t nbytes, uint8_t devAddr, uint32_t stop) {
+I2CSTATUS I2C_Transmit(I2C_TypeDef* I2Cx, const uint8_t *pBuf, uint32_t nbytes, uint8_t devAddr, uint32_t flags) {
 	uint32_t reg;
 	uint32_t tx_count;
 	uint32_t delay_val = I2C_CalcDelay(I2C_TIMEOUT);
 	volatile uint32_t wait;
 
-	// Configure the slave device address, transfer length and generate START condition
-	reg  = I2Cx->CR2;
-	reg &= ~(I2C_CR2_SADD | I2C_CR2_NBYTES | I2C_CR2_RELOAD | I2C_CR2_AUTOEND | I2C_CR2_RD_WRN | I2C_CR2_START | I2C_CR2_STOP);
-	reg |= (devAddr & I2C_CR2_SADD) | I2C_CR2_START;
-	if (nbytes > I2C_NBYTES_MAX) {
-		// The number of bytes to transmit is greater than the maximum value for
-		// one transfer, in this case the RELOAD bit must be set
-		reg |= (I2C_NBYTES_MAX << 16) | I2C_CR2_RELOAD;
-		tx_count = I2C_NBYTES_MAX;
+	// Clear all flags
+	I2Cx->ICR = I2C_ICR_ALL;
+
+	// Everything regarding to the transmission is in the CR2 register
+	reg = I2Cx->CR2;
+	reg &= ~I2C_CR2_ALL;
+
+	// Slave device address
+	reg |= (devAddr & I2C_CR2_SADD);
+
+	// Whether it need to generate START condition
+	if (!(flags & I2C_TX_NOSTART)) { reg |= I2C_CR2_START; }
+
+	// Whether it need to generate STOP condition
+	if ((flags & I2C_TX_CONT) || (nbytes > I2C_NBYTES_MAX)) {
+		reg |= I2C_CR2_RELOAD;
 	} else {
-		reg |= (nbytes << 16) | (stop & I2C_CR2_AUTOEND);
-		tx_count = nbytes;
+		if (!(flags & I2C_TX_NOSTOP)) { reg |= I2C_CR2_AUTOEND; }
 	}
+
+	// Transfer length
+	tx_count = (nbytes > I2C_NBYTES_MAX) ? I2C_NBYTES_MAX : nbytes;
+	nbytes -= tx_count;
+	reg |= tx_count << I2C_CR2_NBYTES_Pos;
+
+	// Write a composed value to the I2C register
 	I2Cx->CR2 = reg;
 
 	// Transmit data
-	while (nbytes) {
+	while (tx_count) {
 		// Wait until either TXIS or NACK flag is set
 		wait = delay_val;
 		while (!((reg = I2Cx->ISR) & (I2C_ISR_TXIS | I2C_ISR_NACKF)) && --wait);
-		if ((reg & I2C_ISR_NACKF) || (wait == 0))  {
-			// Clear the NACK flag
-			I2Cx->ICR = I2C_ICR_NACKCF;
-			return I2C_ERROR;
-		}
+		if ((reg & I2C_ISR_NACKF) || (wait == 0))  { return I2C_ERROR; }
 
 		// Transmit byte
 		I2Cx->TXDR = *pBuf++;
-		nbytes--;
 		tx_count--;
 
 		if ((tx_count == 0) && (nbytes != 0)) {
@@ -189,38 +205,26 @@ I2CSTATUS I2C_Transmit(I2C_TypeDef* I2Cx, const uint8_t *pBuf, uint32_t nbytes, 
 			while (!(I2Cx->ISR & I2C_ISR_TCR) && --wait);
 			if (wait == 0) { return I2C_ERROR; }
 
-			// Is this last part of buffer?
+			// Configure next (or last) portion transfer
 			reg = I2Cx->CR2;
-			if (nbytes > I2C_NBYTES_MAX) {
-				reg |= (I2C_NBYTES_MAX << 16) | I2C_CR2_RELOAD;
-				tx_count = I2C_NBYTES_MAX;
+			reg &= ~(I2C_CR2_NBYTES | I2C_CR2_RELOAD | I2C_CR2_AUTOEND);
+			if ((flags & I2C_TX_CONT) || (nbytes > I2C_NBYTES_MAX)) {
+				reg |= I2C_CR2_RELOAD;
 			} else {
-				reg &= ~(I2C_CR2_RELOAD | I2C_CR2_NBYTES);
-				reg |= (nbytes << 16) | (stop & I2C_CR2_AUTOEND);
-				tx_count = nbytes;
+				if (!(flags & I2C_TX_NOSTOP)) { reg |= I2C_CR2_AUTOEND; }
 			}
+			tx_count = (nbytes > I2C_NBYTES_MAX) ? I2C_NBYTES_MAX : nbytes;
+			nbytes -= tx_count;
+			reg |= tx_count << I2C_CR2_NBYTES_Pos;
 			I2Cx->CR2 = reg;
 		}
 	}
 
-	// End of transmit
+	// End of transmission
 	wait = delay_val;
-	if (stop == I2C_GENSTOP_YES) {
-		// With AUTOEND bit set the STOP condition generated automatically
-		// Wait for STOP flag is set
-		while (!(I2Cx->ISR & I2C_ISR_STOPF) && --wait);
+	while (!(I2Cx->ISR & (I2C_ISR_TC | I2C_ISR_TCR | I2C_ISR_STOPF)) && --wait);
 
-		// Clear the STOP flag
-		I2Cx->ICR = I2C_ICR_STOPCF;
-	} else {
-		// The STOP condition is not generated, wait for TC (Transfer Complete) flag
-		while (!(I2Cx->ISR & I2C_ISR_TC) && --wait);
-	}
-
-	// Reset configuration register
-	I2Cx->CR2 &= ~(I2C_CR2_SADD | I2C_CR2_HEAD10R | I2C_CR2_NBYTES | I2C_CR2_RELOAD | I2C_CR2_RD_WRN);
-
-	return I2C_SUCCESS;
+	return (wait) ? I2C_SUCCESS : I2C_ERROR;
 }
 
 // Receive an amount of data in master mode
@@ -233,70 +237,70 @@ I2CSTATUS I2C_Transmit(I2C_TypeDef* I2Cx, const uint8_t *pBuf, uint32_t nbytes, 
 //   I2C_ERROR if there was a timeout during I2C operations, I2C_SUCCESS otherwise
 I2CSTATUS I2C_Receive(I2C_TypeDef* I2Cx, uint8_t *pBuf, uint32_t nbytes, uint8_t devAddr) {
 	uint32_t reg;
-	uint32_t tx_count;
+	uint32_t rx_count;
 	uint32_t delay_val = I2C_CalcDelay(I2C_TIMEOUT);
 	volatile uint32_t wait;
 
-	// Configure the slave device address, transfer length and generate START condition
-	reg  = I2Cx->CR2;
-	reg &= ~(I2C_CR2_SADD | I2C_CR2_NBYTES | I2C_CR2_RELOAD | I2C_CR2_AUTOEND | I2C_CR2_RD_WRN | I2C_CR2_START | I2C_CR2_STOP);
+	// Clear all flags
+	I2Cx->ICR = I2C_ICR_ALL;
+
+	// Everything regarding to the transmission is in the CR2 register
+	reg = I2Cx->CR2;
+	reg &= ~I2C_CR2_ALL;
+
+	// Configure slave device address, enable START condition and set direction to READ
 	reg |= (devAddr & I2C_CR2_SADD) | I2C_CR2_START | I2C_CR2_RD_WRN;
+
+	// Transfer length
 	if (nbytes > I2C_NBYTES_MAX) {
-		// The number of bytes to transmit is greater than the maximum value for
-		// one transfer, in this case the RELOAD bit must be set
-		reg |= (I2C_NBYTES_MAX << 16) | I2C_CR2_RELOAD;
-		tx_count = I2C_NBYTES_MAX;
+		rx_count = I2C_NBYTES_MAX;
+		reg |= I2C_CR2_RELOAD;
 	} else {
-		reg |= (nbytes << 16) | I2C_CR2_AUTOEND;
-		tx_count = nbytes;
+		rx_count = nbytes;
+		reg |= I2C_CR2_AUTOEND;
 	}
+	reg |= rx_count << I2C_CR2_NBYTES_Pos;
+	nbytes -= rx_count;
+
+	// Write a composed value to the I2C register
 	I2Cx->CR2 = reg;
 
 	// Receive data
-	while (nbytes) {
+	while (rx_count) {
 		// Wait until either RXNE or NACK flag is set
 		wait = delay_val;
 		while (!((reg = I2Cx->ISR) & (I2C_ISR_RXNE | I2C_ISR_NACKF)) && --wait);
-		if ((reg & I2C_ISR_NACKF) || (wait == 0)) {
-			I2Cx->ICR = I2C_ICR_NACKCF;
-			return I2C_ERROR;
-		}
+		if ((reg & I2C_ISR_NACKF) || (wait == 0)) { return I2C_ERROR; }
 
 		// Read received data
 		*pBuf++ = I2Cx->RXDR;
-		nbytes--;
-		tx_count--;
+		rx_count--;
 
-		if ((tx_count == 0) && (nbytes != 0)) {
+		if ((rx_count == 0) && (nbytes != 0)) {
 			// Wait until TCR flag is set (Transfer Complete Reload)
 			wait = delay_val;
 			while (!(I2Cx->ISR & I2C_ISR_TCR) && --wait);
 			if (wait == 0) { return I2C_ERROR; }
 
-			// Is this last part of buffer?
+			// Configure next (or last) portion transfer
 			reg = I2Cx->CR2;
+			reg &= ~(I2C_CR2_NBYTES | I2C_CR2_AUTOEND | I2C_CR2_RELOAD);
 			if (nbytes > I2C_NBYTES_MAX) {
-				reg |= (I2C_NBYTES_MAX << 16) | I2C_CR2_RELOAD;
-				tx_count = I2C_NBYTES_MAX;
+				rx_count = I2C_NBYTES_MAX;
+				reg |= I2C_CR2_RELOAD;
 			} else {
-				reg &= ~(I2C_CR2_RELOAD | I2C_CR2_NBYTES);
-				reg |= (nbytes << 16) | I2C_CR2_AUTOEND;
-				tx_count = nbytes;
+				rx_count = nbytes;
+				reg |= I2C_CR2_AUTOEND;
 			}
+			reg |= rx_count << I2C_CR2_NBYTES_Pos;
+			nbytes -= rx_count;
 			I2Cx->CR2 = reg;
 		}
 	}
 
-	// With AUTOEND bit set the STOP condition generated automatically
-	// Wait for STOP flag is set
+	// Wait for the STOP flag
 	wait = delay_val;
 	while (!(I2Cx->ISR & I2C_ISR_STOPF) && --wait);
 
-	// Clear the STOP flag
-	I2Cx->ICR = I2C_ICR_STOPCF;
-
-	// Reset configuration register
-	I2Cx->CR2 &= ~(I2C_CR2_SADD | I2C_CR2_HEAD10R | I2C_CR2_NBYTES | I2C_CR2_RELOAD | I2C_CR2_RD_WRN);
-
-	return I2C_SUCCESS;
+	return (wait) ? I2C_SUCCESS : I2C_ERROR;
 }
--- stm32l4-template/periph/i2c.h
+++ stm32l4-template/periph/i2c.h
@@ -2,7 +2,7 @@
 #define __I2C_H
 
 
-#include <stm32l4xx.h>
+#include "stm32l4xx.h"
 #include "rcc.h"
 #include "gpio.h"
 
@@ -45,16 +45,42 @@
 #define I2C3_SDA_GPIO_PIN       GPIO_PIN_1
 #define I2C3_SDA_GPIO_SRC       GPIO_PinSource1
 
+
 // Definitions of I2C analog filter state
 #define I2C_AF_ENABLE          ((uint32_t)0x00000000U) // Analog filter is enabled
 #define I2C_AF_DISABLE         I2C_CR1_ANFOFF          // Analog filter is disabled
 
-// Definitions for STOP generation after transfer
-#define I2C_GENSTOP_YES        I2C_CR2_AUTOEND         // Generate STOP after transfer
-#define I2C_GENSTOP_NO         ((uint32_t)0x00000000U) // Don't generate STOP after transfer
+// Flags definitions for transmit function
+#define I2C_TX_STOP            ((uint32_t)0x00000000U) // Generate STOP condition
+#define I2C_TX_NOSTOP          ((uint32_t)0x10000000U) // Don't generate STOP condition
+#define I2C_TX_NOSTART         ((uint32_t)0x20000000U) // Don't generate START condition
+#define I2C_TX_CONT            ((uint32_t)0x40000000U) // The transmission will be continued
+// Definitions for compatibility with old code using this library
+#define I2C_GENSTOP_YES        I2C_TX_STOP
+#define I2C_GENSTOP_NO         I2C_TX_NOSTOP
+
+// Definition of bits to reset in CR2 register
+#define I2C_CR2_ALL            (I2C_CR2_SADD    | \
+								I2C_CR2_NBYTES  | \
+								I2C_CR2_RELOAD  | \
+								I2C_CR2_AUTOEND | \
+								I2C_CR2_RD_WRN  | \
+								I2C_CR2_START   | \
+								I2C_CR2_STOP)
+
+// Definition of all bits in ICR register (clear all I2C flags at once)
+#define I2C_ICR_ALL            (I2C_ICR_ADDRCF  | \
+								I2C_ICR_ALERTCF | \
+								I2C_ICR_ARLOCF  | \
+								I2C_ICR_BERRCF  | \
+								I2C_ICR_NACKCF  | \
+								I2C_ICR_OVRCF   | \
+								I2C_ICR_PECCF   | \
+								I2C_ICR_STOPCF  | \
+								I2C_ICR_TIMOUTCF)
 
 
-// Result of I2C procedures
+// Result of I2C functions
 typedef enum {
 	I2C_ERROR   = 0,
 	I2C_SUCCESS = !I2C_ERROR
@@ -77,13 +103,6 @@ __STATIC_FORCEINLINE void I2C_Disable(I2C_TypeDef* I2Cx) {
 	I2Cx->CR1 &= ~I2C_CR1_PE;
 }
 
-// Clear ADDR flag
-// input:
-//   I2Cx - pointer to the I2C peripheral (I2C1, etc.)
-__STATIC_FORCEINLINE void I2C_ClearFlag_ADDR(I2C_TypeDef* I2Cx) {
-	I2Cx->ICR = I2C_ICR_ADDRCF;
-}
-
 // Configure I2C noise filters
 // input:
 //   I2Cx - pointer to the I2C peripheral (I2C1, etc.)
@@ -94,7 +113,7 @@ __STATIC_FORCEINLINE void I2C_ClearFlag_ADDR(I2C_TypeDef* I2Cx) {
 // note: must be called only when I2C is disabled (PE bit in I2C_CR1 register is reset)
 __STATIC_INLINE void I2C_ConfigFilters(I2C_TypeDef* I2Cx, uint32_t af, uint32_t df) {
 	I2Cx->CR1 &= ~(I2C_CR1_ANFOFF | I2C_CR1_DNF);
-	I2Cx->CR1 |= (af & I2C_CR1_ANFOFF) | ((df << 8) & I2C_CR1_DNF);
+	I2Cx->CR1 |= (af & I2C_CR1_ANFOFF) | ((df << I2C_CR1_DNF_Pos) & I2C_CR1_DNF);
 }
 
 // Configure the I2C timings (SDA setup/hold time and SCL high/low period)
@@ -126,7 +145,7 @@ __STATIC_FORCEINLINE void I2C_GenStop(I2C_TypeDef* I2Cx) {
 // Function prototypes
 void I2C_Init(I2C_TypeDef* I2Cx);
 I2CSTATUS I2C_IsDeviceReady(I2C_TypeDef* I2Cx, uint8_t devAddr, uint32_t Trials);
-I2CSTATUS I2C_Transmit(I2C_TypeDef* I2Cx, const uint8_t *pBuf, uint32_t nbytes, uint8_t devAddr, uint32_t stop);
+I2CSTATUS I2C_Transmit(I2C_TypeDef* I2Cx, const uint8_t *pBuf, uint32_t nbytes, uint8_t devAddr, uint32_t flags);
 I2CSTATUS I2C_Receive(I2C_TypeDef* I2Cx, uint8_t *pBuf, uint32_t nbytes, uint8_t devAddr);
 
 #endif // __I2C_H
