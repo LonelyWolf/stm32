5a7cfe87d930120fce546614c42e16f3b3ca2ff0
Dmitry Semernin <dsemernin@gmail.com>
2021-09-07 03:14:13 +0300

    max44009: cosmetic changes

--- max44009/src/max44009.c
+++ max44009/src/max44009.c
@@ -1,16 +1,44 @@
 #include "max44009.h"
 
 
+// MAX44009 register definitions
+#define MAX44009_REG_INTS                      ((uint8_t)0x00) // Interrupt status
+#define MAX44009_REG_INTE                      ((uint8_t)0x01) // Interrupt enable
+#define MAX44009_REG_CFG                       ((uint8_t)0x02) // Configuration
+#define MAX44009_REG_LUXH                      ((uint8_t)0x03) // Lux reading high byte
+#define MAX44009_REG_LUXL                      ((uint8_t)0x04) // Lux reading low byte
+#define MAX44009_REG_THU                       ((uint8_t)0x05) // Upper threshold high byte
+#define MAX44009_REG_THL                       ((uint8_t)0x06) // Lower threshold high byte
+#define MAX44009_REG_THT                       ((uint8_t)0x07) // Threshold timer
+
+// MAX44009 interrupt configuration bits
+#define MAX44009_INT_ENABLE                    ((uint8_t)0x01) // Interrupt enabled
+#define MAX44009_INT_DISABLE                   ((uint8_t)0x00) // Interrupt disabled
+
+// MAX44009 interrupt status bits
+#define MAX44009_INT_SET                       ((uint8_t)0x01) // Interrupt is asserted
+
+// MAX44009 CFG register bits
+#define MAX44009_CFG_CONT                      ((uint8_t)0x80) // Continuous mode
+#define MAX44009_CFG_MANUAL                    ((uint8_t)0x40) // Manual configuration
+
+// Bit mask for MODE and RANGE bits
+#define MAX44009_MASK_CFG_MODE                 ((uint8_t)0xC0)
+// Bit mask for CDR bit and integration time
+#define MAX44009_MASK_CDR_IT                   ((uint8_t)0x0F)
+
+
+
 // Write new value to the MAX44009 register
 // input:
 //   addr - register address
 //   value - new register value
-static void MAX44009_WriteReg(uint8_t addr, uint8_t value) {
+static void __reg_write(uint8_t addr, uint8_t value) {
 	uint8_t buf[2];
 
 	buf[0] = addr;
 	buf[1] = value;
-	I2C_Transmit(MAX44009_I2C_PORT, buf, 2U, MAX44009_ADDR, I2C_GENSTOP_YES);
+	I2C_Transmit(MAX44009_I2C_PORT, buf, 2U, MAX44009_ADDR, I2C_TX_STOP);
 }
 
 // Read value of the MAX44009 register
@@ -18,33 +46,31 @@ static void MAX44009_WriteReg(uint8_t addr, uint8_t value) {
 //   addr - register address
 // return:
 //   register value
-static uint8_t MAX44009_ReadReg(uint8_t addr) {
+static uint8_t __reg_read(uint8_t addr) {
 	uint8_t value = 0U;
 
-	I2C_Transmit(MAX44009_I2C_PORT, &addr, 1U, MAX44009_ADDR, I2C_GENSTOP_NO);
+	I2C_Transmit(MAX44009_I2C_PORT, &addr, 1U, MAX44009_ADDR, I2C_TX_NOSTOP);
 	I2C_Receive(MAX44009_I2C_PORT, &value, 1U, MAX44009_ADDR);
 
 	return value;
 }
 
 // Read MAX44009 interrupt status
-// return:
-//   MAX44009_INT_RESET - no interrupt trigger event has occurred
-//   MAX44009_INT_SET - interrupt has been triggered
+// return: nonzero value if interrupt has been triggered
 // note: INTS bit will be cleared after calling this function
 uint8_t MAX44009_INTStatus(void) {
-	return (MAX44009_ReadReg(MAX44009_REG_INTS) & MAX44009_INT_SET);
+	return (__reg_read(MAX44009_REG_INTS) & MAX44009_INT_SET) == MAX44009_INT_SET;
 }
 
 // Enable interrupt on INT pin
 void MAX44009_INTEnable(void) {
-	MAX44009_WriteReg(MAX44009_REG_INTE, MAX44009_INT_ENABLE);
+	__reg_write(MAX44009_REG_INTE, MAX44009_INT_ENABLE);
 }
 
 // Disable interrupt on INT pin
 // note: also clears INTS bit in the STATUS register
 void MAX44009_INTDisable(void) {
-	MAX44009_WriteReg(MAX44009_REG_INTE, MAX44009_INT_DISABLE);
+	__reg_write(MAX44009_REG_INTE, MAX44009_INT_DISABLE);
 }
 
 // Configure IC for full automatic mode
@@ -53,9 +79,9 @@ void MAX44009_INTDisable(void) {
 void MAX44009_SetModeAutomatic(void) {
 	uint8_t reg;
 
-	reg = MAX44009_ReadReg(MAX44009_REG_CFG);
+	reg = __reg_read(MAX44009_REG_CFG);
 	reg &= (uint8_t)~(MAX44009_CFG_CONT | MAX44009_CFG_MANUAL);
-	MAX44009_WriteReg(MAX44009_REG_CFG, reg);
+	__reg_write(MAX44009_REG_CFG, reg);
 }
 
 // Configure IC for continuous mode
@@ -68,10 +94,10 @@ void MAX44009_SetModeAutomatic(void) {
 void MAX44009_SetModeContinuous(void) {
 	uint8_t reg;
 
-	reg = MAX44009_ReadReg(MAX44009_REG_CFG);
+	reg = __reg_read(MAX44009_REG_CFG);
 	reg |= MAX44009_CFG_CONT;
 	reg &= (uint8_t)~MAX44009_CFG_MANUAL;
-	MAX44009_WriteReg(MAX44009_REG_CFG, reg);
+	__reg_write(MAX44009_REG_CFG, reg);
 }
 
 // Configure IC for manual mode
@@ -83,12 +109,12 @@ void MAX44009_SetModeContinuous(void) {
 void MAX44009_SetModeManual(uint8_t cdr, uint8_t tim) {
 	uint8_t reg;
 
-	reg = MAX44009_ReadReg(MAX44009_REG_CFG);
+	reg = __reg_read(MAX44009_REG_CFG);
 	// Clear CONT, CDR and TIM bits of CFG register
 	reg &= (uint8_t)~(MAX44009_CFG_CONT | MAX44009_CDR_DIV8 | MAX44009_IT_6d25ms);
 	// Set MANUAL bit and configure new values of CDR and TIM[2:0] bits
 	reg |= (uint8_t)(MAX44009_CFG_MANUAL | (cdr & MAX44009_CDR_DIV8) | (tim & MAX44009_IT_6d25ms));
-	MAX44009_WriteReg(MAX44009_REG_CFG, reg);
+	__reg_write(MAX44009_REG_CFG, reg);
 }
 
 // Configure MAX44009 upper threshold
@@ -96,7 +122,7 @@ void MAX44009_SetModeManual(uint8_t cdr, uint8_t tim) {
 //   value - new value of the upper threshold
 // note: for the explanation of that value refer to the datasheet
 void MAX44009_SetThrU(uint8_t value) {
-	MAX44009_WriteReg(MAX44009_REG_THU, value);
+	__reg_write(MAX44009_REG_THU, value);
 }
 
 // Configure MAX44009 lower threshold
@@ -104,7 +130,7 @@ void MAX44009_SetThrU(uint8_t value) {
 //   value - new value of the lower threshold
 // note: for the explanation of that value refer to the datasheet
 void MAX44009_SetThrL(uint8_t value) {
-	MAX44009_WriteReg(MAX44009_REG_THL, value);
+	__reg_write(MAX44009_REG_THL, value);
 }
 
 // Configure MAX44009 threshold timer
@@ -112,7 +138,7 @@ void MAX44009_SetThrL(uint8_t value) {
 //   value - new value of the threshold timer
 // note: for the explanation of that value refer to the datasheet
 void MAX44009_SetThrT(uint8_t value) {
-	MAX44009_WriteReg(MAX44009_REG_THT, value);
+	__reg_write(MAX44009_REG_THT, value);
 }
 
 // Configure MAX44009 to its default state
@@ -122,7 +148,7 @@ void MAX44009_Init(void) {
 	// Default mode: measurements performed every 800ms
 	// Auto range: CDR and integration time are automatically determined by
 	//             the internal autoranging circuitry of the IC
-	MAX44009_WriteReg(MAX44009_REG_CFG, 0x03U);
+	__reg_write(MAX44009_REG_CFG, 0x03U);
 	// Upper threshold: maximum
 	MAX44009_SetThrU(0xFFU);
 	// Lower threshold: minimum
@@ -143,10 +169,10 @@ uint32_t MAX44009_GetLux(void) {
 	uint8_t lhb; // Lux high byte
 
 	// Get RAW lux readings
-	lhb = MAX44009_ReadReg(MAX44009_REG_LUXH);
+	lhb = __reg_read(MAX44009_REG_LUXH);
 #if (MAX44009_ACCURACY)
 	uint8_t llb; // Lux low byte
-	llb = MAX44009_ReadReg(MAX44009_REG_LUXL);
+	llb = __reg_read(MAX44009_REG_LUXL);
 #endif
 
 	// Calculate lux value using formula: 2^(exponent) * mantissa * 0.045
